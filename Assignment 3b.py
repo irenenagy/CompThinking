4678b4f9f630af3a3c5dae046d4d2875521ed99b
irenenagy <144862815+irenenagy@users.noreply.github.com>
2023-09-25 21:39:27 +0900

    Add files via upload

---
+++ Assignment 3b.py
@@ -0,0 +1,17 @@
+score = int(input("Enter the test score ==> "))
+if score >=90:
+    grade = 'A'
+elif score >=80:
+    grade = 'B'
+elif score >=70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'F' 
+    print("Grade is %s." %grade) 
+ValueError;
+print("Error.Incorrect Value.")    
+    
+        
+        
